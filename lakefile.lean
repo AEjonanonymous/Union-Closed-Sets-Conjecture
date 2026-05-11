8b013d03d86a11d6311442ca39a95e6ed7365e09
Jonathan ƒ(n) Reed <jonathanalanreed1983@gmail.com>
2026-05-11 17:21:39 -0400

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -5,6 +5,3 @@ package "union-closed-sets-conjecture"
 
 @[default_target]
 lean_lib «Frankl's_Conjecture_Proof»
-
-require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "v4.29.0"
