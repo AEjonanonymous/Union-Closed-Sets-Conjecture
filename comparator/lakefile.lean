51de7c76e7f516c758d059eb9cf2d716948eda19
Jonathan ƒ(n) Reed <jonathanalanreed1983@gmail.com>
2026-09-11 23:00:17 -0400

    Create lakefile.lean

---
+++ comparator/lakefile.lean
@@ -0,0 +1,14 @@
+import Lake
+open Lake DSL
+
+package «UnionClosedSetsConjecture» where
+
+require mathlib from git 
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.29.0"
+
+@[default_target]
+lean_lib «Challenge» where
+  srcDir := "."
+
+lean_lib «Solution» where
+  srcDir := "."
